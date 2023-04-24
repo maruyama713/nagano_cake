da428a72ee0e73447f2a78825c0d1bc0155b476a
EC2 Default User <ec2-user@ip-172-31-3-245.ap-northeast-1.compute.internal>
2023-04-24 07:02:10 +0000

    [Update] admins,customersのsign_up,sign_inページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,9 @@
 class Public::CustomersController < ApplicationController
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
