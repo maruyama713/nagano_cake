ae5366fe8427951c432bc2cf2280012410f8c1d6
EC2 Default User <ec2-user@ip-172-31-3-245.ap-northeast-1.compute.internal>
2023-05-08 07:14:12 +0000

    [Add] カート内商品編集機能

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,8 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :order_items, dependent: :destroy
   
   def active_for_authentication?
     super && (is_deleted == false)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
+  has_many :order_items, dependent: :destroy
   
   def with_tax_price
     (price * 1.1).floor
