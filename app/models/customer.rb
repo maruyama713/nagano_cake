e9fa86fa8d3784312639cd9a401213482479b981
EC2 Default User <ec2-user@ip-172-31-3-245.ap-northeast-1.compute.internal>
2023-05-09 07:45:25 +0000

    [Update] カート画面

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,7 @@ class Customer < ApplicationRecord
          
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
-  has_many :order_items, dependent: :destroy
+  
   
   def active_for_authentication?
     super && (is_deleted == false)
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
-  belongs_to :customer
+  belongs_to :order
   belongs_to :item
+
+  def subtotal
+    item.with_tax_price * amount
+  end
 end
