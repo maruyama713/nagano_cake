d4bd686a5dfbb54205e83af5a3a9031f49d76946
EC2 Default User <ec2-user@ip-172-31-3-245.ap-northeast-1.compute.internal>
2023-05-13 09:33:09 +0000

    [Update] 注文情報確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,24 +9,17 @@ class Public::OrdersController < ApplicationController
     @total = @cart_items.inject(0){|sum,item| sum + item.subtotal}
     @billing_amount = @total + @postage
     if params[:order][:method_of_payment] == "0"
-      @method_of_payment = [:credit_card]
+      @method_of_payment = :credit_card
     elsif params[:order][:method_of_payment] == "1"
-      @method_of_payment = [:transfer]
-    if params[:order][:select_addresses] == "0"
+      @method_of_payment = :transfer
+    end
+    if params[:order][:select_address] == '0'
       @order = Order.new(order_params)
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.address_name = current_customer.first_name + current_customer.last_name
-    elsif params[:order][:select_addresses] == "1"
+    elsif params[:order][:select_address] == '1'
       @order = Order.new(order_params)
-      @address = Address.find(params[:order][:address_id])
-      @order.postal_code = @address.postal_code
-      @order.address = @address.address
-      @order.address_name = @address.name
-    elsif params[:order][:select_addresses] == "2"
-      @order = Order.new(order_params)
-    else
-      render 'new'
     end
   end
 
@@ -38,6 +31,6 @@ class Public::OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:method_of_payment, :postal_code, :address, :address_name)
+    params.require(:order).permit(:select_address, :method_of_payment, :postal_code, :address, :address_name)
   end
 end
