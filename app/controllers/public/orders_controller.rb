240575ae9270eae0229d975b48a644a31b0bad8f
EC2 Default User <ec2-user@ip-172-31-3-245.ap-northeast-1.compute.internal>
2023-05-13 04:35:08 +0000

    [Update] 注文情報確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,12 +2,16 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
-  
+
   def confirmation
     @cart_items = current_customer.cart_items.all
     @postage = 800
     @total = @cart_items.inject(0){|sum,item| sum + item.subtotal}
     @billing_amount = @total + @postage
+    if params[:order][:method_of_payment] == "0"
+      @method_of_payment = [:credit_card]
+    elsif params[:order][:method_of_payment] == "1"
+      @method_of_payment = [:transfer]
     if params[:order][:select_addresses] == "0"
       @order = Order.new(order_params)
       @order.postal_code = current_customer.postal_code
@@ -25,13 +29,13 @@ class Public::OrdersController < ApplicationController
       render 'new'
     end
   end
-  
+
   def index
   end
-  
+
   def show
   end
-  
+
   private
   def order_params
     params.require(:order).permit(:method_of_payment, :postal_code, :address, :address_name)
