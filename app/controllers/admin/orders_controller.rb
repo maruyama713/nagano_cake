c20ccd79ecc8a33715109ad68a0154ab2c6e52ad
EC2 Default User <ec2-user@ip-172-31-3-245.ap-northeast-1.compute.internal>
2023-05-19 07:11:12 +0000

    [Update] admin/orders show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,6 +3,6 @@ class Admin::OrdersController < ApplicationController
     @customer = Customer.find(params[:id])
     @order = Order.find(params[:id])
     @order_items = @customer.order_ids
-    @order_total = @cart_items.inject(0){|sum,item| sum + item.subtotal}
+    @order_total = @order_items.inject(0){|sum,item| sum + item.subtotal}
   end
 end
